f2e6837093697f26b5b3d48d9f9907216968f691
Willian Fonseca <willian.alves2109@gmail.com>
2022-10-29 16:55:53 -0300

    INICIANDO PROJETO

---
+++ imprimindo_variaveis.py
@@ -0,0 +1,23 @@
+# VARIAVEIS
+
+nome = "Willian"
+sobrenome = "Fonseca"
+idade = "29"
+
+# IMPRIMINDO VARIAVEIS
+
+print("Olá, meu nome é", nome, sobrenome, "e eu tenho", idade, "anos.")
+
+print(f"Olá, o meu nome é {nome} {sobrenome} e eu tenho {idade} anos.")
+
+# ENTRADA DE DADOS
+
+nome1 = input("Digite o seu nome: ")
+sobrenome1 = input("Digite o seu sobre nome: ")
+idade1 = int(input('Digite a sua idade: '))
+
+print(f"Olá, o meu nome é {nome1} {sobrenome1} e eu tenho {idade1} anos.")
+
+
+
+
